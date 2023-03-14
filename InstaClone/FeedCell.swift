59b6a6fa45aa0d148c53df11a4c24f12b31e3fea
aliberkayberber <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>
2023-03-14 21:02:25 +0300

    Feed VC developed

--- InstaClone/FeedCell.swift
+++ InstaClone/FeedCell.swift
@@ -6,13 +6,14 @@
 //
 
 import UIKit
-
+import Firebase
 class FeedCell: UITableViewCell {
     
     @IBOutlet weak var userEmailLabel: UILabel!
     @IBOutlet weak var commentLabel: UILabel!
     @IBOutlet weak var userImageView: UIImageView!
     
+    @IBOutlet weak var documentIdLabel: UILabel!
     
     @IBOutlet weak var likeLabel: UILabel!
     
@@ -29,6 +30,14 @@ class FeedCell: UITableViewCell {
 
     
     @IBAction func likeClicked(_ sender: Any) {
+        let fireStoreDatabase = Firestore.firestore()
+        
+        if let likeCount = Int(likeLabel.text!) {
+            let likeStore = ["likes": likeCount + 1] as [String : Any]
+            
+            fireStoreDatabase.collection("Posts").document(documentIdLabel.text!).setData(likeStore, merge: true)
+        }
+        
     }
     
 }
--- InstaClone/FeedViewController.swift
+++ InstaClone/FeedViewController.swift
@@ -6,36 +6,90 @@
 //
 
 import UIKit
-
+import Firebase
+import SDWebImage
 class FeedViewController: UIViewController {
 
     
     @IBOutlet weak var tableView: UITableView!
+    
+    var userEmailArray = [String]()
+    var userCommentArray = [String]()
+    var likeArray = [Int]()
+    var userImageArray = [String]()
+    var documentIDArray = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         tableView.delegate = self
         tableView.dataSource = self
+        
+        getDataFromFireStore()
+    }
+    func makeAlert(inputTitle: String , inputMassage: String) {
+        let alert = UIAlertController(title: inputTitle, message: inputMassage, preferredStyle: UIAlertController.Style.alert)
+        let action = UIAlertAction(title: "OK", style: UIAlertAction.Style.default)
+        alert.addAction(action)
+        self.present(alert, animated: true)
     }
     
+    func getDataFromFireStore() {
+        let fireStoreDatabase = Firestore.firestore()
+        
+        fireStoreDatabase.collection("Posts").order(by: "date", descending: true)
+            .addSnapshotListener { snapShot, error in
+            if error != nil {
+                self.makeAlert(inputTitle: "Error", inputMassage: error?.localizedDescription ?? "Error!")
+            } else {
+                if snapShot?.isEmpty != true {
+                    self.userImageArray.removeAll()
+                    self.userEmailArray.removeAll()
+                    self.userCommentArray.removeAll()
+                    self.likeArray.removeAll()
+                    self.documentIDArray.removeAll()
+                    for document in snapShot!.documents {
+                      let documentID =  document.documentID
+                        self.documentIDArray.append(documentID)
+                        
+                        if  let postedBy = document.get("postedBy") as? String {
+                            self.userEmailArray.append(postedBy)
+                        }
+                        if let postComment = document.get("postComment") as? String {
+                            self.userCommentArray.append(postComment)
+                        }
+                        if let likes = document.get("likes") as? Int {
+                            self.likeArray.append(likes)
+                        }
+                        if let imageUrl = document.get("imageUrl") as? String {
+                            self.userImageArray.append(imageUrl)
+                        }
+                    }
+                    self.tableView.reloadData()
+                }
+                
+            }
+        }
+        
+        
+    }
 }
 
 extension FeedViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return userImageArray.count
     }
 }
 
 extension FeedViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! FeedCell
-        cell.likeLabel.text = "0"
-        cell.commentLabel.text = "My comment"
-        cell.userEmailLabel.text = "user@email.com"
-        cell.userImageView.image = UIImage(named: "selectImage.png")
+        cell.likeLabel.text =   String(likeArray[indexPath.row])
+        cell.commentLabel.text = userCommentArray[indexPath.row]
+        cell.userEmailLabel.text = userEmailArray[indexPath.row]
+        cell.userImageView.sd_setImage(with: URL(string: self.userImageArray[indexPath.row]))
+        cell.documentIdLabel.text = documentIDArray[indexPath.row]
         return cell
     }
-    
     
 }
